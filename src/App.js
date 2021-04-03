cf21888945c58c3a057020b35c6a28800f8ed124
Rolando Lopez <rolandolopez3442@gmail.com>
2021-04-03 07:59:18 -0700

    revert to single App file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
-import { FlatList, StatusBar, StyleSheet, Switch, View } from 'react-native'
+import {
+  FlatList,
+  StatusBar,
+  StyleSheet,
+  Switch,
+  View,
+  TouchableOpacity,
+  Text,
+} from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
-import { State } from 'react-native-ble-plx'
 import { Header, ListItem, ThemeProvider } from 'react-native-elements'
+import { State } from 'react-native-ble-plx'
+import Peripheral, { Characteristic, Service } from 'react-native-peripheral'
 import scanner from './scanner'
 
 const DEVICE_LIST_LIMIT = 50
@@ -12,6 +21,39 @@ const App = () => {
   const [error, setError] = useState('-')
   const [started, setStarted] = useState(false)
   const [devices, setDevices] = useState([])
+  const [value, setValue] = useState('')
+
+  const startAdvertising = useCallback(() => {
+    Peripheral.onStateChanged(async (state) => {
+      if (state === 'poweredOn') {
+        const serviceUuid = 'ebed0e09-033a-4bfe-8dcc-20a04fad944e'
+        const char1 = new Characteristic({
+          uuid: 'c36e1c5a-fc6e-48c8-9a8e-d0b350399d0e',
+          value: 'aGVsbG8=',
+        })
+        const char2 = new Characteristic({
+          uuid: 'fbc47809-76ce-44fa-a2f0-676b95615472',
+          onReadRequest: async () => value,
+          onWriteRequest: async (val) => setValue(val),
+        })
+        try {
+          await Peripheral.addService(
+            new Service({
+              uuid: serviceUuid,
+              characteristics: [char1, char2],
+            })
+          )
+          Peripheral.startAdvertising({
+            name: 'I love cats',
+            serviceUuids: [serviceUuid],
+          })
+          console.log('bluetooth is working')
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    })
+  })
 
   const { start, stop, observe } = useMemo(() => scanner(), [])
 
@@ -70,6 +112,11 @@ const App = () => {
               />
             }
           />
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={startAdvertising} style={styles.button}>
+              <Text>StartAdvertising</Text>
+            </TouchableOpacity>
+          </View>
           <View style={styles.statusPanel}>
             <ListItem bottomDivider>
               <ListItem.Content>
@@ -88,12 +135,12 @@ const App = () => {
             <FlatList
               style={styles.list}
               data={devices}
-              renderItem={({ device, index }) => (
+              renderItem={({ item, index }) => (
                 <ListItem bottomDivider key={index}>
                   <ListItem.Title>
-                    {device.name || 'Unknown Device'}
+                    {item.name || 'Unknown Device'}
                   </ListItem.Title>
-                  <ListItem.Subtitle>RSSI: {device.rssi}</ListItem.Subtitle>
+                  <ListItem.Subtitle>RSSI: {item.rssi}</ListItem.Subtitle>
                 </ListItem>
               )}
               keyExtractor={(_, index) => index.toString()}
@@ -115,6 +162,15 @@ const styles = StyleSheet.create({
   },
   list: {
     flex: 1,
+  },
+  button: {
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#ddd',
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    padding: 10,
   },
 })
 
